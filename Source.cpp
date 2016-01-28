aea70982436ac609775f015721ed86252dbb3442
Jacob White <jacob.white@byu.net>
2016-01-28 08:50:51 -0700

    Nothing is working.

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int val[3];
 
-enum Action { none, binarize, canny, lines, diff, harris, pretzel, otsu, hsvHue, hsvSat, hsvVal};
+enum Action { none, binarize, canny, action_lines, diff, harris, pretzel, otsu, hsvHue, hsvSat, hsvVal, c0, c1, c2};
 Action action = none;
 
 RNG rng(12345);
@@ -18,12 +18,12 @@ RNG rng(12345);
 #define RIGHT_ARROW 2555904
 #define DOWN_ARROW 2621440
 #define MIN_IMG_NUMBER 1
-#define MAX_IMG_NUMBER 15
+#define MAX_IMG_NUMBER 16
 #define STARTING_IMG_NUMBER 1
 
-Mat img, hsv;
+Mat img, imgCropped, imgHsv;
 int imageNumber;
-Mat result, frame2;
+Mat imgResult, frame2;
 
 Point2i ballCenter(260, 175);
 int roiAbove = 20;
@@ -36,6 +36,64 @@ void loadImage(int imageNumber)
 	sprintf(name, "img (%d).bmp", imageNumber);
 	string str_name(name);
 	img = imread(str_name);
+}
+
+void showHistogram(Mat& img)
+{
+	int bins = 256;             // number of bins
+	int nc = img.channels();    // number of channels
+
+	vector<Mat> hist(nc);       // histogram arrays
+
+								// Initalize histogram arrays
+	for (int i = 0; i < hist.size(); i++)
+		hist[i] = Mat::zeros(1, bins, CV_32SC1);
+
+	// Calculate the histogram of the image
+	for (int i = 0; i < img.rows; i++)
+	{
+		for (int j = 0; j < img.cols; j++)
+		{
+			for (int k = 0; k < nc; k++)
+			{
+				uchar val = nc == 1 ? img.at<uchar>(i, j) : img.at<Vec3b>(i, j)[k];
+				hist[k].at<int>(val) += 1;
+			}
+		}
+	}
+
+	// For each histogram arrays, obtain the maximum (peak) value
+	// Needed to normalize the display later
+	int hmax[3] = { 0,0,0 };
+	for (int i = 0; i < nc; i++)
+	{
+		for (int j = 0; j < bins - 1; j++)
+			hmax[i] = hist[i].at<int>(j) > hmax[i] ? hist[i].at<int>(j) : hmax[i];
+	}
+
+	const char* wname[3] = { "blue", "green", "red" };
+	Scalar colors[3] = { Scalar(255,0,0), Scalar(0,255,0), Scalar(0,0,255) };
+
+	vector<Mat> canvas(nc);
+
+	// Display each histogram in a canvas
+	for (int i = 0; i < nc; i++)
+	{
+		canvas[i] = Mat::ones(125, bins, CV_8UC3);
+
+		for (int j = 0, rows = canvas[i].rows; j < bins - 1; j++)
+		{
+			line(
+				canvas[i],
+				Point(j, rows),
+				Point(j, rows - (hist[i].at<int>(j) * rows / hmax[i])),
+				nc == 1 ? Scalar(200, 200, 200) : colors[i],
+				1, 8, 0
+				);
+		}
+
+		imshow(nc == 1 ? "value" : wname[i], canvas[i]);
+	}
 }
 
 void pressKey(int key)
@@ -65,7 +123,7 @@ void pressKey(int key)
 	else if (key == 'c')
 		action = canny;
 	else if (key == 'l')
-		action = lines;
+		action = action_lines;
 	else if (key == 'd')
 		action = diff;
 	else if (key == 'h')
@@ -74,6 +132,12 @@ void pressKey(int key)
 		action = hsvSat;
 	else if (key == 'v')
 		action = hsvVal;
+	else if (key == '0')
+		action = c0;
+	else if (key == '1')
+		action = c1;
+	else if (key == '2')
+		action = c2;
 	//else if (key == 's')
 	//	imwrite("test.png", result);
 	else if (key != -1)
@@ -117,8 +181,8 @@ void calcCrop(Size& imgSize, vector<Vec2f>& lines, vector<Point2f>& pts, Rect& r
 	for (int i = 0; i < lines.size(); i++)
 	{
 		float rho = lines[i][0], theta = lines[i][1];
-		Point2f p1 = calcRectIntersection(img.size(), rho, theta, true);
-		Point2f p2 = calcRectIntersection(img.size(), rho, theta, false);
+		Point2f p1 = calcRectIntersection(imgCropped.size(), rho, theta, true);
+		Point2f p2 = calcRectIntersection(imgCropped.size(), rho, theta, false);
 		if (p1.x < imgSize.width / 2)
 		{
 			if (p1.x > roiX1)
@@ -139,7 +203,7 @@ void calcCrop(Size& imgSize, vector<Vec2f>& lines, vector<Point2f>& pts, Rect& r
 		pts.push_back(p1);
 		pts.push_back(p2);
 	}
-	Rect roi2(roiX1, 0, roiX2 - roiX1, img.size().height);
+	Rect roi2(roiX1, 0, roiX2 - roiX1, imgCropped.size().height);
 	roi = roi2;
 }
 
@@ -148,8 +212,10 @@ void drawCrop(Mat& img, vector<Point2f> pts, Rect& roi)
 	for (int i = 0; i < pts.size(); i += 2)
 	{
 		line(img, pts[i], pts[i + 1], pts[i].x > img.size().width / 2 ? Scalar(0, 0, 255) : Scalar(255, 0, 0), 1, CV_AA);
+		printf("draw line\n");
 	}
 	rectangle(img, roi, Scalar(0, 255, 0), 1, CV_AA);
+	imshow("drawCrop", img);
 }
 
 void getChannel(Mat& img, Mat& imgChannel, int channel)
@@ -174,43 +240,118 @@ void setHsv(Mat& before, Mat& hsv, Mat& after, int channel)
 	cvtColor(hsv, after, CV_HSV2BGR);
 }
 
-void hueDist(Mat& hsv, Mat& dist, int hueVal)
+void channelDist(Mat& hsv, Mat& dist, int hueVal, int channel)
 {
-	Mat hue;
-	getChannel(hsv, hue, 0);
-	absdiff(hue, hueVal, dist);
+	Mat channelImg;
+	getChannel(hsv, channelImg, channel);
+	if (channel == 0)
+	{
+		Mat dist1, dist2;
+		absdiff(channelImg, hueVal, dist1);
+		absdiff(channelImg, hueVal + ((hueVal < 90) ? 180 : -180), dist2);
+		min(dist1, dist2, dist);
+	}
+	else
+		absdiff(channelImg, hueVal, dist);
 }
 
 void processImage()
 {
-	//Process image using chosen settings
+
+// Crop the image
+	/// Convert the image to grayscale
+	Mat gray;
+	cvtColor(img, gray, CV_BGR2GRAY);
+
+	Mat detectedEdges;
+	/// Reduce noise with a kernel 3x3
+	blur(gray, detectedEdges, Size(3, 3));
+
+	/// Canny detector
+	int val2 = 30;
+	Canny(detectedEdges, detectedEdges, val2, val2 * 3, 3);
+
+	vector<Vec2f> lines;
+	HoughLines(detectedEdges, lines, 1, CV_PI / 180, 150);
+
+	Mat edgesC;
+	cvtColor(detectedEdges, edgesC, CV_GRAY2BGR);
+	imgResult = edgesC;
+
+	vector<Point2f> pts;
+	Rect roi;
+	calcCrop(img.size(), lines, pts, roi);
+
+	Mat img2(img, roi);
+
+	if(!img2.empty())
+		imshow("img2", img2);
+
+	//if (imageNumber != 16)
+		imgCropped = img2;
+	//else
+	//	imgCropped = img;
+
+//Process image using chosen settings
 	if (action == none)
-		result = img;
+	{
+		imgResult = imgCropped;
+		printf("none - imgResult = imgCropped\n");
+	}
 	else if (action == binarize)
 	{
 		Mat result_gray, hsv;
-		cvtColor(img, hsv, CV_BGR2HSV);
+		cvtColor(imgCropped, hsv, CV_BGR2HSV);
 		inRange(hsv, Scalar(val[0], val[1], val[2]), Scalar(255, 255, 255), result_gray);
-		cvtColor(result_gray, result, CV_GRAY2BGR);
+		cvtColor(result_gray, imgResult, CV_GRAY2BGR);
 	}
 	else if (action == hsvHue)
-		setHsv(img, hsv, result, 0);
+		setHsv(imgCropped, imgHsv, imgResult, 0);
 	else if (action == hsvSat)
-		setHsv(img, hsv, result, 1);
+		setHsv(imgCropped, imgHsv, imgResult, 1);
 	else if (action == hsvVal)
-		setHsv(img, hsv, result, 2);
+		setHsv(imgCropped, imgHsv, imgResult, 2);
 	else if (action == otsu)
 	{
 		// Thresholding image
 		Mat gray, bw;
-		cvtColor(img, gray, CV_BGR2GRAY);
+		cvtColor(imgCropped, gray, CV_BGR2GRAY);
 		cv::threshold(gray, bw, 60, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-		result = bw;
+		imgResult = bw;
+	}
+	else if (action == c0)
+	{
+		cvtColor(imgCropped, imgHsv, CV_BGR2HSV);
+		channelDist(imgHsv, imgResult, 16, 0);
+		Mat c;
+		getChannel(imgHsv, c, 0);
+		showHistogram(c);
+	}
+	else if (action == c1)
+	{
+		cvtColor(imgCropped, imgHsv, CV_BGR2HSV);
+		Mat result;
+		getChannel(imgHsv, result, 1);
+		showHistogram(result);
+	}
+	else if (action == c2)
+	{
+		cvtColor(imgCropped, imgHsv, CV_BGR2HSV);
+		channelDist(imgHsv, imgResult, 255, 2);
+		Mat c;
+		getChannel(imgHsv, c, 2);
+		showHistogram(c);
 	}
 	else if (action == pretzel)
 	{
-		cvtColor(img, hsv, CV_BGR2HSV);
-		hueDist(hsv, result, 16);
+		cvtColor(imgCropped, imgHsv, CV_BGR2HSV);
+		Mat d1, d2;
+		channelDist(imgHsv, d1, 16, 0);
+
+
+
+
+
 
 		
 
@@ -246,7 +387,7 @@ void processImage()
 	{
 		// Convert the image to grayscale
 		Mat gray;
-		cvtColor(img, gray, CV_BGR2GRAY);
+		cvtColor(imgCropped, gray, CV_BGR2GRAY);
 
 		Mat detectedEdges;
 		// Reduce noise with a kernel 3x3
@@ -254,52 +395,29 @@ void processImage()
 
 		// Canny detector
 		Canny(detectedEdges, detectedEdges, val[0], val[0] * 3, 3);
-		result = detectedEdges;
+		imgResult = detectedEdges;
 	}
-	else if (action == lines)
+	else if (action == action_lines)
 	{
-		/// Convert the image to grayscale
-		Mat gray;
-		cvtColor(img, gray, CV_BGR2GRAY);
-
-		Mat detectedEdges;
-		/// Reduce noise with a kernel 3x3
-		blur(gray, detectedEdges, Size(3, 3));
-
-		/// Canny detector
-		int val2 = 30;
-		Canny(detectedEdges, detectedEdges, val2, val2 * 3, 3);
-
-		vector<Vec2f> lines;
-		HoughLines(detectedEdges, lines, 1, CV_PI / 180, 150);
-		Mat edgesC;
-		cvtColor(detectedEdges, edgesC, CV_GRAY2BGR);
-		result = edgesC;
-
-		vector<Point2f> pts;
-		Rect roi;
-		calcCrop(img.size(), lines, pts, roi);
-
-		drawCrop(result, pts, roi);
-
-		//Mat croppedImg(img, roi);
-		//result = croppedImg;		
+		imgResult = edgesC;
+		drawCrop(imgResult, pts, roi);
+		printf("%d lines, %d points, %d %d %d %d roi\n", lines.size(), pts.size(), roi.x, roi.y, roi.width, roi.height);
 	}
 	else if (action == diff)
 	{
 		if (frame2.data)
 		{
-			result = img.clone(); // Mat::zeros(frame.size(), frame.type());
-			absdiff(img, frame2, result);
+			imgResult = imgCropped.clone(); // Mat::zeros(frame.size(), frame.type());
+			absdiff(imgCropped, frame2, imgResult);
 		}
-		frame2 = img.clone();
+		frame2 = imgCropped.clone();
 	}
 	else if (action == harris)
 	{
 		/// Convert the image to grayscale
 		Mat gray;
 		Mat dst, dst_norm, dst_norm_scaled;
-		cvtColor(img, gray, CV_BGR2GRAY);
+		cvtColor(imgCropped, gray, CV_BGR2GRAY);
 		if (val[0] < 1)
 			val[0] = 1;
 
@@ -313,7 +431,7 @@ void processImage()
 
 		/// Copy the source image
 		Mat copy;
-		copy = img.clone();
+		copy = imgCropped.clone();
 
 		/// Apply corner detection
 		goodFeaturesToTrack(gray, corners, val[0], qualityLevel,
@@ -326,7 +444,7 @@ void processImage()
 			circle(copy, corners[i], r, Scalar(rng.uniform(0, 255), rng.uniform(0, 255),
 				rng.uniform(0, 255)), -1, 8, 0);
 		}
-		result = copy;
+		imgResult = copy;
 
 		/// Set the neeed parameters to find the refined corners
 		Size winSize = Size(5, 5);
@@ -342,7 +460,10 @@ void processImage()
 		//	cout << " -- Refined Corner [" << i << "]  (" << corners[i].x << "," << corners[i].y << ")" << endl;
 		//}
 	}
-	imshow("White", result);
+	if (!imgResult.empty())
+		imshow("White", imgResult);
+	else
+		printf("Empty\n");
 }
 
 void trackbarCallback(int, void*)
@@ -355,15 +476,15 @@ void mouseCallback(int event, int x, int y, int flags, void* userdata)
 {
 	if (event == EVENT_LBUTTONDOWN)
 	{
-		if (hsv.empty())
+		if (imgHsv.empty())
 		{
 			printf("empty\n");
 			return;
 		}
-		Vec3b v = hsv.at<Vec3b>(y, x);
+		Vec3b v = imgHsv.at<Vec3b>(y, x);
 		printf("%d %d %d\n", v[0], v[1], v[2]);
 		Mat dist;
-		hueDist(hsv, dist, hsv.at<Vec3b>(y, x)[0]);
+		channelDist(imgHsv, dist, imgHsv.at<Vec3b>(y, x)[0], 0);
 		imshow("White", dist);
 	}
 }
